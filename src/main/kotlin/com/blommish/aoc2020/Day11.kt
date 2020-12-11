3b6a054b12e55a5bd40adb335cf01c358cd93d77
Johan Blomgren <johan.blomgren@nav.no>
2020-12-12 00:03:11 +0100

    Cleaned up day 11

--- src/main/kotlin/com/blommish/aoc2020/Day11.kt
+++ src/main/kotlin/com/blommish/aoc2020/Day11.kt
@@ -2,7 +2,6 @@ package com.blommish.aoc2020
 
 import com.blommish.aoc2020.util.Day
 import com.blommish.aoc2020.util.withLines
-import java.util.stream.IntStream
 
 fun main() {
     withLines(::Day11, "11_test.txt", "11_1.txt")
@@ -10,83 +9,55 @@ fun main() {
 
 private class Day11(val lines: List<String>) : Day {
 
+    val next = listOf(Pair(1, 0), Pair(1, -1), Pair(1, 1),
+                      Pair(0, -1), Pair(0, 1),
+                      Pair(-1, 0), Pair(-1, -1), Pair(-1, 1))
+
     override fun first(): Int {
-        var seats = lines.map { line -> line.toCharArray() }.toTypedArray()
-        fun isEmpty(column: Int, rowNum: Int): Boolean {
-            val row = seats.getOrNull(rowNum)
-            return row == null || (row.getOrNull(column) != '#' &&
-                                   row.getOrNull(column - 1) != '#' &&
-                                   row.getOrNull(column + 1) != '#')
-        }
-
-        fun isOccupied(array: CharArray, column: Int): Int = array.getOrNull(column)?.let { if (it == '#') 1 else 0 } ?: 0
-
-        fun countOccupied(column: Int, row: Int): Int {
-            return IntStream.of(row - 1, row, row + 1)
-                    .map { rowNum ->
-                        seats.getOrNull(rowNum)?.let { seat ->
-                            (if (row != rowNum) isOccupied(seat, column) else 0) +
-                            isOccupied(seat, column - 1) + isOccupied(seat, column + 1)
-                        } ?: 0
-                    }
-                    .sum()
-        }
-
-        var hasChanged: Boolean
-        while (true) {
-            hasChanged = false
-            seats = seats.mapIndexed { row, rowSeats ->
-                rowSeats.mapIndexed inner@{ column, seat ->
-                    if (seat == 'L' && isEmpty(column, row) && isEmpty(column, row + 1) && isEmpty(column, row - 1)) {
-                        hasChanged = true
-                        return@inner '#'
-                    } else if (seat == '#' && countOccupied(column, row) > 3) {
-                        hasChanged = true
-                        return@inner 'L'
-                    }
-                    return@inner seat
-                }.toCharArray()
-            }.toTypedArray()
-            if (!hasChanged) {
-                return seats.sumOf { it.count { it == '#' } }
+        fun isTaken(seats: Array<CharArray>, x: Int, y: Int, nextX: Int, nextY: Int): Boolean {
+            val row = seats.getOrNull(y + nextY) ?: return false
+            val seat = row.getOrNull(x + nextX) ?: return false
+            return when (seat) {
+                '#' -> true
+                else -> false
             }
         }
-    }
-
-    fun nextSeat(column: Int, row: Int, columnNext: Int, rowNext: Int, seats: Array<CharArray>, returnValue: Boolean): Boolean {
-        val rowSeats = seats.getOrNull(row + rowNext) ?: return returnValue
-        val seat = rowSeats.getOrNull(column + columnNext) ?: return returnValue
-        if (seat == 'L') return returnValue
-        if (seat == '#') return !returnValue
-        return nextSeat(column + columnNext, row + rowNext, columnNext, rowNext, seats, returnValue)
+        return calc(3) { seats, next, column, row -> isTaken(seats, column, row, next.second, next.first) }
     }
 
     override fun second(): Int {
+        fun isTaken(seats: Array<CharArray>, x: Int, y: Int, nextX: Int, nextY: Int): Boolean {
+            val row = seats.getOrNull(y + nextY) ?: return false
+            val seat = row.getOrNull(x + nextX) ?: return false
+            return when (seat) {
+                '#' -> true
+                'L' -> false
+                else -> isTaken(seats, x + nextX, y + nextY, nextX, nextY)
+            }
+        }
+        return calc(4) { seats, next, column, row -> isTaken(seats, column, row, next.second, next.first) }
+    }
+
+    private fun calc(takenSeats: Int, isTaken: (Array<CharArray>, Pair<Int, Int>, Int, Int) -> Boolean): Int {
         var seats = lines.map { line -> line.toCharArray() }.toTypedArray()
-
-        val next = listOf(Pair(1, 0), Pair(1, -1), Pair(1, 1),
-                          Pair(0, -1), Pair(0, 1),
-                          Pair(-1, 0), Pair(-1, -1), Pair(-1, 1))
-
-        fun isEmpty(column: Int, row: Int): Boolean =
-                next.all { nextSeat(column, row, it.first, it.second, seats, true) }
-
-        fun countOccupied(column: Int, row: Int): Int =
-                next.count { nextSeat(column, row, it.first, it.second, seats, false) }
-
         var hasChanged: Boolean
         while (true) {
             hasChanged = false
             seats = seats.mapIndexed { row, rowSeats ->
                 rowSeats.mapIndexed inner@{ column, seat ->
-                    if (seat == 'L' && isEmpty(column, row)) {
-                        hasChanged = true
-                        '#'
-                    } else if (seat == '#' && countOccupied(column, row) > 4) {
-                        hasChanged = true
-                        'L'
-                    } else {
+                    return@inner if (seat == '.') {
                         seat
+                    } else {
+                        val count = next.count { next -> isTaken.invoke(seats, next, column, row) }
+                        if (seat == 'L' && count == 0) {
+                            hasChanged = true
+                            '#'
+                        } else if (seat == '#' && count > takenSeats) {
+                            hasChanged = true
+                            'L'
+                        } else {
+                            seat
+                        }
                     }
                 }.toCharArray()
             }.toTypedArray()
@@ -94,7 +65,6 @@ private class Day11(val lines: List<String>) : Day {
                 return seats.sumOf { it.count { it == '#' } }
             }
         }
-        return 0
     }
 
 }
