b1d1b8b8a37074beda9770e30def7f0175f25621
Johan Blomgren <johan.blomgren@nav.no>
2020-12-14 09:22:35 +0100

    Day 14 Part 2

--- src/main/kotlin/com/blommish/aoc2020/Day14.kt
+++ src/main/kotlin/com/blommish/aoc2020/Day14.kt
@@ -4,43 +4,73 @@ import com.blommish.aoc2020.util.DayT
 import com.blommish.aoc2020.util.withLines
 
 fun main() {
-    withLines(::Day14, { it.split("\n") }, "14_test.txt", "14_1.txt")
+    withLines(::Day14, { it.split("\n") }, "14_test_2.txt", "14_1.txt")
 }
 
 private class Day14(val lines: List<String>) : DayT<Long> {
 
     companion object {
+
         val REGEX = """(\w+)(\[(\d+)\])? = (\w+)""".toRegex()
     }
 
+    //15514035145260
     override fun first(): Long {
         var mask = ""
-        val map = mutableMapOf<String, Long>()
-        lines.forEachIndexed { i, line ->
-            val (operation, _, place, value) = REGEX.matchEntire(line)!!.destructured
+        val map = mutableMapOf<String, CharArray>()
+        lines.forEach { line ->
+            val (operation, _, memoryIndex, value) = REGEX.matchEntire(line)!!.destructured
             if (operation == "mask") {
                 mask = value
             } else {
-                val binary = value.toInt().toString(2).padStart(36, '0')
-                var b = "000000000000000000000000000000000000".toCharArray()
+                val binary = value.toInt().toString(2).padStart(36, '0').toCharArray()
                 for (i in mask.indices) {
                     val c = mask[i]
-                    if (c == '1') {
-                        b[i] = '1'
-                    } else if (c == 'X') {
-                        b[i] = binary[i]
+                    if (c == '1' || c == '0') {
+                        binary[i] = c
                     }
                 }
-                map[place] = b.joinToString("").toLong(2)
+                map[memoryIndex] = binary
             }
         }
-        return map.values.reduce { acc, s ->
-            acc + s
-        }
+        return map.values.map { it.joinToString("").toLong(2) }.sum()
     }
 
+    //3926790061594
     override fun second(): Long {
-        return 0
+        var mask = ""
+        val map = mutableMapOf<String, Long>()
+        lines.forEach { line ->
+            val (operation, _, memoryIndex, value) = REGEX.matchEntire(line)!!.destructured
+            if (operation == "mask") {
+                mask = value
+            } else {
+                val binary = memoryIndex.toInt().toString(2).padStart(36, '0').toCharArray()
+                val floatingIndices = mutableListOf<Int>()
+                for (i in mask.indices) {
+                    val c = mask[i]
+                    if (c == 'X') {
+                        binary[i] = c
+                        floatingIndices.add(i)
+                    } else if (c == '1') {
+                        binary[i] = c
+                    }
+                }
+                val list = mutableListOf(binary)
+                floatingIndices.forEach {
+                    for (i in list.indices) {
+                        val copyOf = list[i].copyOf()
+                        copyOf[it] = '0'
+                        list.add(copyOf)
+                        list[i][it] = '1'
+                    }
+                }
+                list.forEach {
+                    map[it.joinToString("")] = value.toLong()
+                }
+            }
+        }
+        return map.values.sum()
     }
 
 }
